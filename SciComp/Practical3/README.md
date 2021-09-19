f3b03854dc3666edfc1d2611a8c7185bdb10c524
Olga Bystrova <bystrovaolgavl@gmail.com>
2021-09-19 13:15:58 +0300

    practical 3 done

---
+++ SciComp/Practical3/README.md
@@ -0,0 +1,3 @@
+# Interfaces I
+
+[Link to the materials](https://hse-scicomp.gitlab.io/schedule/03-interfaces1.html)
--- SciComp/Practical3/root13.sh
+++ SciComp/Practical3/root13.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
 
-echo "Hello, My name is Olya" | tr '[A-Z]' '[X-ZA-W]'
+LETTERS=abcdefghijklmnopqrstuvwxyz
+N=12
+
+while [[ $message = "" ]]
+do
+  echo -n "What message do you want to process?"
+  read message
+done
+
+output=""
+
+for (( i=0; i<${#message}; i++ )); do
+    letter=${message:$i:1}
+    if [[ $LETTERS =~ $letter ]]; then
+        shift=$(((N + `expr index "$LETTERS" $letter`) % 26))
+        output+=${letter/$letter/${LETTERS:$shift:1}}
+    fi
+done
+
+echo $output
+exit 0
